8a97fc79f740b33d443076665d4a9f8a4d1ea2e2
Aleksi Väisänen <aleksi.vaisanen@elisanet.fi>
2020-09-06 17:09:17 +0300

    fetching and filtering data works

--- script.js
+++ script.js
@@ -1,31 +1,87 @@
-let searchTerms = []
+let state = {
+    searchTerms: [],
+    currentStopNumber: "",
+    line: "",
+}
 
 const firstStopInput = document.getElementById("firstStop");
 firstStopInput.addEventListener('input', event => getStopData(event.target.value));
+const firstLineInput = document.getElementById("firstLine");
+firstLineInput.addEventListener('input', event => updateLineToState(event.target.value));
 
 const getStopData = (stopNumber) => {
-    const validStop = validateStopNumber(stopNumber)
+    state.currentStopNumber = stopNumber;
+    const validStop = validateStopNumber()
+    if (validStop) {
+        fetchStopData(state.currentStopNumber)
+            .then(data => {
+                renderResults(data)
+            })
+    } else if (state.searchTerms[state.currentStopNumber]) {
+        //if there already is data
+        renderResults()
+    }
 }
 
-const validateStopNumber = (stopNumber) => {
+const validateStopNumber = () => {
     let validSizeAndFormat = false
     // size constraint & special cases
-    const firstChar = stopNumber.charAt(0)
-    if (stopNumber.length > 0 && stopNumber.length <= 4 && firstChar !== "0") {
-        if (stopNumber.substring(0, 2) === "PT" && !isNaN(stopNumber.substring(2))) {
+    const firstChar = state.currentStopNumber.charAt(0)
+    if (state.currentStopNumber.length > 0 && state.currentStopNumber.length <= 4 && firstChar !== "0") {
+        if (state.currentStopNumber.substring(0, 2) === "PT" && !isNaN(state.currentStopNumber.substring(2))) {
             validSizeAndFormat = true
-        } else if ((firstChar === "L" || firstChar === "P" || firstChar === "T") && !isNaN(stopNumber.substring(1))) {
+        } else if ((firstChar === "L" || firstChar === "P" || firstChar === "T") && !isNaN(state.currentStopNumber.substring(1))) {
             validSizeAndFormat = true
-        } else if (!isNaN(stopNumber)) {
+        } else if (!isNaN(state.currentStopNumber)) {
             validSizeAndFormat = true
         }
-    } else if (stopNumber === "LTulo") {
+    } else if (state.currentStopNumber === "LTulo") {
         validSizeAndFormat = true
     }
 
-    if (searchTerms[stopNumber]) {
-        console.log("don't fire fetch, client already has data")
+    if (!state.searchTerms[state.currentStopNumber] && validSizeAndFormat) {
+        return true
     } else {
-        searchTerms[stopNumber] = "jeij"
+        return false
     }
+}
+
+const fetchStopData = async function () {
+    try {
+        const response = await fetch("http://data.foli.fi/siri/sm/" + state.currentStopNumber);
+        return response.json();
+    } catch (e) {
+        console.error("Fetch failed", e)
+        return e
+    }
+}
+
+const updateLineToState = (line) => {
+    state.line = line
+    renderResults()
+}
+
+const renderResults = (data = null) => {
+    if (data !== null) {
+        state.searchTerms[state.currentStopNumber] = data
+    } else {
+        data = state.searchTerms[state.currentStopNumber]
+    }
+    const firstStopList = document.getElementById("firstStopList")
+    //clear previous li-items
+    while (firstStopList.firstChild) {
+        firstStopList.removeChild(firstStopList.firstChild)
+    }
+
+    //append new data
+    data.result.slice(0, 10).map(result => {
+        if (result.lineref === state.line || state.line === "") {
+            const arrivalTime = new Date(result.expectedarrivaltime * 1000)
+            const arrivalTimeFormatted = arrivalTime.getHours() + ":" + arrivalTime.getMinutes() + ":" + arrivalTime.getSeconds()
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(result.lineref + " " + arrivalTimeFormatted))
+            li.setAttribute("class", "list-group-item")
+            firstStopList.appendChild(li)
+        }
+    })
 }
